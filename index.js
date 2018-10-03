699b30984829409408b0ec3f4747744de90844c6
Joaquim Barreto <joaquim@barreto.co>
2018-10-03 15:53:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+
 function sayHiToGrandma(string) {
+  if (sayHiToGrandma.toUpperCase() === sayHiToGrandma)
   return "I can't hear you"
 }
