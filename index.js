10d56a3be526afb523bcf043c870c6e5733f5824
Joaquim Barreto <joaquim@barreto.co>
2018-10-03 15:25:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log()
+}
+
 function logWhisper(string) {
   console.log(string.toLowerCase)
 }
